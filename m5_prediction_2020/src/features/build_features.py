1c2b9344bdeeb24c9d252827072535db05b5ceaf
Abhishek Namballa <abhisheknamballa@Abhisheks-MBP.fios-router.home>
2020-05-27 18:42:03 -0400

    Updated build_features.py - segmented to pre, merge and post merge. Need to code add_features method.

--- m5_prediction_2020/src/features/build_features.py
+++ m5_prediction_2020/src/features/build_features.py
@@ -3,6 +3,11 @@ import numpy as np
 import gc
 import utils.utils as utils
 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
 class build_features:
     
     def __init__(self,file_path):
@@ -23,59 +28,83 @@ class build_features:
         self.TRAIN_DTYPES.update({numcol:"float32" for numcol in self.train_numcols})
 
 
-    #def normalize_data(self,df):
-
-
-    def pre_process_data(self,filename):
+    def read_data(self,n_samples = None):
+        #Read in data
+        filename = 'calendar'
+        self.df_cal = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.PRICE_DTYPES)
+        logging.info("Finished reading in {rows} rows and {cols} columns from {name}".format(
+            rows = self.df_cal.shape[0],cols = self.df_cal.shape[1],name = filename))
         
-        if filename == 'sell_prices':
-            df = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.PRICE_DTYPES)
-            for col, col_dtype in self.PRICE_DTYPES.items():
-                if col_dtype == "category":
-                    df[col] = df[col].cat.codes.astype("int16")
-                    df[col] -= df[col].min()
-                    
-        if filename == 'sales_train_validation':
-            
-            df = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.TRAIN_DTYPES)
-
-            for col in self.train_catcols:
-                if col != "id":
-                    df[col] = df[col].cat.codes.astype("int16")
-                    df[col] -= df[col].min()
-            
-            df = df.assign(id=df.id.str.replace("_validation", ""))
-            df = df.reindex(columns=df.columns.tolist() + ["d_" + str(1913 + i + 1) for i in range(2 * 28)])
-            df = df.melt(id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
-                 var_name='d', value_name='demand')
-            df = df.assign(d=df.d.str[2:].astype("int16"))
-
         
-        if filename == 'calendar':
-            df = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.CAL_DTYPES)
-            df["date"] = pd.to_datetime(df["date"])
-            for col, col_dtype in self.CAL_DTYPES.items():
-                if col_dtype == "category":
-                    df[col] = df[col].cat.codes.astype("int16")
-                    df[col] -= df[col].min()
-            df = df.assign(d=df.d.str[2:].astype("int16"))
-            
-        return df
+        filename = 'sell_prices'
+        self.df_sell = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.PRICE_DTYPES)
+        logging.info("Finished reading in {rows} rows and {cols} columns from {name}".format(
+            rows = self.df_sell.shape[0],cols = self.df_sell.shape[1],name = filename))
+        
+        
+        filename = 'sales_train_validation'
+        self.df_sales = pd.read_csv(self.file_path + '/' + filename + '.csv', dtype = self.PRICE_DTYPES, nrows = n_samples)
+        logging.info("Finished reading in {rows} rows and {cols} columns from {name}".format(
+            rows = self.df_sales.shape[0],cols = self.df_sales.shape[1],name = filename))
+        
 
-    def build_master(self,df_sales, df_cal, df_prices,n_sample = None):
-        if not n_sample:
-            df_master = df_sales.merge(df_cal, how="left", on="d")
-        else:
-            #Change functionality so that this picks n_samples of a particular id
-            #More intuitive to work with in that way
-            df_master = df_sales.sample(n_sample).merge(df_cal, how="left", on="d")
+    def process_data_pre_merge(self):
+        
+        self.df_sales = self.df_sales.assign(id=self.df_sales.id.str.replace("_validation", ""))
+        self.df_sales = self.df_sales.reindex(columns=self.df_sales.columns.tolist() + ["d_" + str(1913 + i + 1) for i in range(2 * 28)])
+        self.df_sales = self.df_sales.melt(id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
+                var_name='d', value_name='demand')
+        self.df_sales = self.df_sales.assign(d=self.df_sales.d.str[2:].astype("int16"))
 
-        df_master = df_master.merge(df_prices, how="left", on=["wm_yr_wk", "store_id", "item_id"])
-        df_master.drop(['wm_yr_wk'], axis = 1, inplace = True)
+        #For calendar data
+        self.df_cal["date"] = pd.to_datetime(self.df_cal["date"])
+        self.df_cal = self.df_cal.assign(d=self.df_cal.d.str[2:].astype("int16"))
+    
+        logging.info("Processed pre-merge...")
+    
+    def build_master(self):
+        df_master = self.df_sales.merge(self.df_cal, how="left", on="d")
+        df_master = df_master.merge(self.df_sell, how="left", on=["wm_yr_wk", "store_id", "item_id"])
+        
 
+        self.df_master = df_master
+
+        logging.info("Processed merge - Master dataframe has {rows} rows and {cols} columns".format(
+            rows = self.df_master.shape[0],cols = self.df_master.shape[1]))
+        
         gc.collect()
 
-        return df_master
+    
+    def process_data_post_merge(self):
+
+        #Drop year,wday,wm_yr_wk
+        self.df_master.drop(['wm_yr_wk','year','weekday'], axis = 1, inplace = True)
+
+        #Encode categorical columns
+        catcols = ['dept_id','store_id', 'cat_id', 'state_id','event_name_1',
+            'event_type_1','event_name_2','event_type_2','snap_CA','snap_TX','snap_WI']
+        
+        for col in catcols:
+            self.df_master[col] = self.df_master[col].astype('category')
+            self.df_master[col] = self.df_master[col].cat.codes.astype("int16")
+            self.df_master[col] -= self.df_master[col].min()
+
+
+        logging.info("Processed post-merge...")
+
+        return self.df_master
+
+        gc.collect()
+    
+    #def add_features(self):
+    
+    def process_data(self,n_samples = None):
+        self.read_data(n_samples)
+        self.process_data_pre_merge()
+        self.build_master()
+        return self.process_data_post_merge()
+
+
 
 
 if __name__ == '__main__':
@@ -83,23 +112,12 @@ if __name__ == '__main__':
     path = r'/Users/abhisheknamballa/Desktop/Kaggle/m5_prediction/kaggle-m5-accuracy-2020/m5_prediction_2020/data/'
     b_fea = build_features(path + 'raw/')
     
-    df_cal = b_fea.pre_process_data('calendar')
-    print('Processed calendar data')
-    df_prices = b_fea.pre_process_data('sell_prices')
-    print('Processed sales price data')
-    df_sales = b_fea.pre_process_data('sales_train_validation')
-    print('Processed sales qnty data')
-
-    df_master = utils.reduce_mem_usage(b_fea.build_master(df_sales,df_cal,df_prices, n_sample=10))
-    print('Processed merge')
-
-    del df_cal
-    del df_prices
-    del df_sales
-
+    
+    df_master = b_fea.process_data(10)
+    
     gc.collect()
     
-    #df_master.to_feather(path + 'external/master_dataframe')
+    df_master.to_csv(path + 'external/master_dataframe')
 
 
         
